f9a5f914c1d6cf957edc8abceb0d7c974eb7ef68
Trinh Gliedt <chuot2008@gmail.com>
2021-08-03 10:39:03 -0700

    8/3/21

---
+++ 06_React_Javascript/03_Ecommerce_Redux_SimpleTut/redux_ecommerce/src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBOL4Uh-rge6NMsi9bzNg1fDoD4s9DiNgg",
+  authDomain: "reduxecom.firebaseapp.com",
+  projectId: "reduxecom",
+  storageBucket: "reduxecom.appspot.com",
+  messagingSenderId: "749067529699",
+  appId: "1:749067529699:web:43740612d434715ccf04d6",
+};
---
+++ 06_React_Javascript/03_Ecommerce_Redux_SimpleTut/v0/src/pages/Registration/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Signup from "../../components/Signup";
+import "./styles.scss";
+
+class Registration extends Component {
+  render() {
+    return (
+      <div>
+        <Signup />
+      </div>
+    );
+  }
+}
+export default Registration;
